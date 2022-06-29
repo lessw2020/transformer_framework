cf50be19a1a49f4894b76954990e9e7dd808d9f7
lessw2020 <lessw@etrillium.com>
2022-06-29 10:52:28 -0700

    updates to save checkpoint

--- model_checkpointing/checkpoint_handler.py
+++ model_checkpointing/checkpoint_handler.py
@@ -1,13 +1,13 @@
 from pathlib import Path
-from datetime import datetime 
+from datetime import datetime
 import torch
 
 from torch.distributed.fsdp import (
     FullyShardedDataParallel as FSDP,
     StateDictType,
-    FullStateDictConfig, # general model non-sharded, non-flattened params
-    LocalStateDictConfig, # flattened params, usable only by FSDP
-    #ShardedStateDictConfig, # un-flattened param but shards, usable by other parallel schemes.
+    FullStateDictConfig,  # general model non-sharded, non-flattened params
+    LocalStateDictConfig,  # flattened params, usable only by FSDP
+    # ShardedStateDictConfig, # un-flattened param but shards, usable by other parallel schemes.
 )
 
 from torch.distributed._shard.checkpoint import (
@@ -17,6 +17,7 @@ from torch.distributed._shard.checkpoint import (
     load_state_dict,
 )
 
+
 def get_date_of_run():
     """create date and time for file save uniqueness
     example: 2022-05-07-08:31:12_PM'
@@ -25,25 +26,45 @@ def get_date_of_run():
     print(f"--> current date and time of run = {date_of_run}")
     return date_of_run
 
+
 # create singleton saving policies to avoid making over and over
 fullstate_save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
 
-def save_checkpoint(model, rank, cfg, epoch=1, verbose=True):
-    """ saving model via rank0 cpu streaming"""
-    #nonlocal fullstate_save_policy
-    #fullstate_save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
+
+def save_checkpoint(
+    model, rank, cfg, epoch=1, curr_metric=None, best_metric=None, verbose=True
+):
+    """saving model via rank0 cpu streaming or distributed checkpointing"""
+    # if a metric is passed in, confirm if we want to checkpoint based on new low or not
+    # and update best metric
+    if curr_metric and best_metric:
+        if curr_metric > best_metric:
+            if verbose and rank == 0:
+                print(
+                    f"--> New record for curr_metric not established.  Not saving checkpoint "
+                )
+            return
+        best_metric = curr_metric
+
+    # confirm type of checkpoint and save
     if cfg.checkpoint_type == StateDictType.LOCAL_STATE_DICT:
         # create writer to current path
-        save_dir = Path.cwd()/'dist_checkpoint'
+        save_dir = Path.cwd() / cfg.checkpoint_folder
         writer = FileSystemWriter(save_dir)
-            
-        with FSDP.state_dict_type(model, StateDictType.LOCAL_STATE_DICT, ):
+
+        with FSDP.state_dict_type(
+            model,
+            StateDictType.LOCAL_STATE_DICT,
+        ):
             state_dict = model.state_dict()
 
         save_state_dict(state_dict, writer)
-        
+        if rank == 0:
+            print(f"--> distributed checkpoint saved at {save_dir}")
+
         return  # we break to avoid hitting default full state
 
+    # saving with rank0 cpu
     with FSDP.state_dict_type(
         model, StateDictType.FULL_STATE_DICT, fullstate_save_policy
     ):
@@ -54,21 +75,24 @@ def save_checkpoint(model, rank, cfg, epoch=1, verbose=True):
 
     if rank == 0:
         print(f"--> saving model ...")
-        
+        save_dir = Path.cwd() / cfg.checkpoint_folder
         save_name = "test" + "-" + ".pt"
-
+        save_dir += save_name
         torch.save(cpu_state, save_name)
 
         print(f"--> saved {save_name} to disk")
 
-                
+
 def load_checkpoint(model, rank, cfg, verbose=True):
     """load local checkpoint to rank0 cpu"""
-    if cfg.checkpoint_type==StateDictType.LOCAL_STATE_DICT:
-        checkdir = Path.cwd()/'dist_checkpoint'
+    if cfg.checkpoint_type == StateDictType.LOCAL_STATE_DICT:
+        checkdir = Path.cwd() / "dist_checkpoint"
         reader = FileSystemReader(checkdir)
 
-        with FSDP.state_dict_type(model, StateDictType.LOCAL_STATE_DICT,) :
+        with FSDP.state_dict_type(
+            model,
+            StateDictType.LOCAL_STATE_DICT,
+        ):
             state_dict = model.state_dict()
             load_state_dict(state_dict, reader)
             model.load_state_dict(state_dict)
@@ -76,9 +100,9 @@ def load_checkpoint(model, rank, cfg, verbose=True):
         print(f"--> local state loaded on rank {rank}")
     return
 
-    if rank !=0:
+    if rank != 0:
         return
-    
+
     full_state_dict = torch.load(cfg.checkpoint_name)
     model.load_state_dict(full_state_dict)
     if verbose:
