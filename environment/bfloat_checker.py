c7efc3af5ed0b598b168d1a0fc0859cfdb692e3a
lessw2020 <lessw@etrillium.com>
2022-07-15 22:26:29 +0000

    remove tutorial items

--- environment/bfloat_checker.py
+++ environment/bfloat_checker.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2022 Meta Platforms, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the Apache-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This is a simple check to confirm that your current server has full bfloat support -
+#  both GPU native support, and Network communication support.
+
+# Be warned that if you run on V100 without a check like this, you will be running without native Bfloat16
+# support and will find significant performance degradation (but it will not complain via an error).
+# Hence the reason for a checker!
+
 from pkg_resources import packaging
 import torch
 import torch.cuda.nccl as nccl
