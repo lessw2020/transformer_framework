d593dc2074f194fc67e363c453ab6d89451bcfe6
linjianma <malinjianev@gmail.com>
2022-07-18 04:36:10 +0000

    Refactor main_training and add t5_config

---
+++ config/base_config.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+from torch.distributed.fsdp import (
+    ShardingStrategy,
+    BackwardPrefetch,
+)
+
+@dataclass
+class base_config:
+
+    # seed
+    seed: int = 2022
+    verbose: bool = True  # how much info to show...
+    # how many mini batches to time with
+    total_steps_to_run: int = 5
+
+    # training
+    batch_size_training: int = 4
+    num_epochs: int = 1
+
+    # sharding policy
+    sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD
+    print_sharding_plan: bool = False
+
+    run_profiler: bool = False
+
+    # backward prefetch
+    backward_prefetch = BackwardPrefetch.BACKWARD_PRE
+
+    # log
+    log_every: int = 1
+
+    # dataloaders
+    num_workers_dataloader: int = 2
+
+    # policies
+    use_mixed_precision: bool = True
+
+    # activation checkpointing
+    fsdp_activation_checkpointing: bool = True
+
+    # validation
+    run_validation: bool = False
+    val_batch_size = 4
+
+    # logging
+    track_memory = True
+    memory_report: bool = True
+    nccl_debug_handler: bool = True
+    distributed_debug: bool = True
