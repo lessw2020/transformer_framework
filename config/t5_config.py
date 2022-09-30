bb6b1558b8af7aba24ac346c629f886480a93e46
lessw2020 <lessw@etrillium.com>
2022-09-30 01:37:31 +0000

    add distributed checkpoint saving for t5

--- config/t5_config.py
+++ config/t5_config.py
@@ -36,9 +36,11 @@ class train_config(base_config):
     use_real_data = True
 
     # checkpoint models
-    save_model_checkpoint: bool = False
-    load_model_checkpoint: bool = False
-    checkpoint_type = StateDictType.FULL_STATE_DICT
+    save_model_checkpoint: bool = True
+    load_model_checkpoint: bool = True
+    checkpoint_type = StateDictType.LOCAL_STATE_DICT
+    dist_checkpoint_root_folder = "distributed_checkpoints"
+    dist_checkpoint_folder = "T5_local_checkpoint"
     model_save_name = "t5-"
     checkpoint_folder = "training_checkpoints"
     checkpoint_max_save_count: int = (
--- run_training4.sh
+++ run_training4.sh
@@ -1,1 +1,1 @@
-torchrun --nnodes=1 --nproc_per_node=4 --rdzv_id=101 --rdzv_endpoint="localhost:5969" main_training.py 
+torchrun --nnodes=1 --nproc_per_node=4 --rdzv_id=101 --rdzv_endpoint="localhost:5969" main_training.py --model t5 
